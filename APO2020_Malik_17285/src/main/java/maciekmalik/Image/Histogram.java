c3035db8b75834590961347a2ad6d4fa36e11b17
Maciek Malik <M.Malik@wit.edu.pl>
2020-04-10 22:56:09 +0200

    hist. stats

--- APO2020_Malik_17285/src/main/java/maciekmalik/Image/Histogram.java
+++ APO2020_Malik_17285/src/main/java/maciekmalik/Image/Histogram.java
@@ -28,14 +28,15 @@ import org.jfree.chart.renderer.category.StandardBarPainter;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.lang.Double;
 
-public class Histogram extends JFrame {
+public class Histogram extends JFrame implements ChangeListener {
 
     /*
     Graph lib:
@@ -57,6 +58,7 @@ public class Histogram extends JFrame {
     }
 
     private boolean isColor = true;
+    private Map<String, int[]> tmoOUT = new HashMap<>();
 
 
     private void initComponents() {
@@ -65,47 +67,67 @@ public class Histogram extends JFrame {
         jPTabRed = new javax.swing.JPanel();
         jPTabGreen = new javax.swing.JPanel();
         jPTabBlue = new javax.swing.JPanel();
+        jLMean = new javax.swing.JLabel();
+        jLStdDev = new javax.swing.JLabel();
+        jLMedian = new javax.swing.JLabel();
+        jLCount = new javax.swing.JLabel();
+        jLPercentile = new javax.swing.JLabel();
+        jLPixels = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPTabRed.setPreferredSize(new java.awt.Dimension(403, 313));
 
         javax.swing.GroupLayout jPTabRedLayout = new javax.swing.GroupLayout(jPTabRed);
         jPTabRed.setLayout(jPTabRedLayout);
         jPTabRedLayout.setHorizontalGroup(
                 jPTabRedLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 386, Short.MAX_VALUE)
+                        .addGap(0, 546, Short.MAX_VALUE)
         );
         jPTabRedLayout.setVerticalGroup(
                 jPTabRedLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 263, Short.MAX_VALUE)
+                        .addGap(0, 346, Short.MAX_VALUE)
         );
 
-        jTabCon.addTab("RED", jPTabRed);
+        jTabCon.addTab("Red", jPTabRed);
 
         javax.swing.GroupLayout jPTabGreenLayout = new javax.swing.GroupLayout(jPTabGreen);
         jPTabGreen.setLayout(jPTabGreenLayout);
         jPTabGreenLayout.setHorizontalGroup(
                 jPTabGreenLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 386, Short.MAX_VALUE)
+                        .addGap(0, 546, Short.MAX_VALUE)
         );
         jPTabGreenLayout.setVerticalGroup(
                 jPTabGreenLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 263, Short.MAX_VALUE)
+                        .addGap(0, 346, Short.MAX_VALUE)
         );
 
-        jTabCon.addTab("GREEN", jPTabGreen);
+        jTabCon.addTab("Green", jPTabGreen);
 
         javax.swing.GroupLayout jPTabBlueLayout = new javax.swing.GroupLayout(jPTabBlue);
         jPTabBlue.setLayout(jPTabBlueLayout);
         jPTabBlueLayout.setHorizontalGroup(
                 jPTabBlueLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 386, Short.MAX_VALUE)
+                        .addGap(0, 546, Short.MAX_VALUE)
         );
         jPTabBlueLayout.setVerticalGroup(
                 jPTabBlueLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 263, Short.MAX_VALUE)
+                        .addGap(0, 346, Short.MAX_VALUE)
         );
 
-        jTabCon.addTab("BLUE", jPTabBlue);
+        jTabCon.addTab("Blue", jPTabBlue);
+
+        jLMean.setText("Mean: 0.000");
+
+        jLStdDev.setText("Std dev: 0.000");
+
+        jLMedian.setText("Median: 0.000");
+
+        jLCount.setText("Count: 0000000");
+
+        jLPercentile.setText("Percentile: 000");
+
+        jLPixels.setText("Pixels: 0000000");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -113,20 +135,54 @@ public class Histogram extends JFrame {
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jTabCon)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jTabCon)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                                                        .addComponent(jLMedian)
+                                                                        .addComponent(jLStdDev))
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                                        .addComponent(jLCount)
+                                                                        .addComponent(jLPercentile)))
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addComponent(jLMean)
+                                                                .addGap(18, 18, 18)
+                                                                .addComponent(jLPixels)))
+                                                .addGap(0, 0, Short.MAX_VALUE)))
                                 .addContainerGap())
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jTabCon)
+                                .addComponent(jTabCon, javax.swing.GroupLayout.DEFAULT_SIZE, 371, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLMean)
+                                        .addComponent(jLPixels))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLCount)
+                                        .addComponent(jLStdDev))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLMedian)
+                                        .addComponent(jLPercentile))
                                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>
 
+    private javax.swing.JLabel jLCount;
+    private javax.swing.JLabel jLMean;
+    private javax.swing.JLabel jLMedian;
+    private javax.swing.JLabel jLPercentile;
+    private javax.swing.JLabel jLPixels;
+    private javax.swing.JLabel jLStdDev;
     private javax.swing.JPanel jPTabBlue;
     private javax.swing.JPanel jPTabGreen;
     private javax.swing.JPanel jPTabRed;
@@ -161,7 +217,7 @@ public class Histogram extends JFrame {
 
         if(image != null){
 
-            this._renderFrame(this._calculate(),title);
+            this._renderFrame(this._calculateHist(),title);
 
         }else{
             System.out.println("Image cannot be empty");
@@ -178,15 +234,72 @@ public class Histogram extends JFrame {
     }
 
 
-    private Map<String, int[]> _calculate(){
+    private void _updateStats(Map<String, Double> map){
+
+        jLCount.setText("Count: " + map.get("count"));
+        jLMean.setText("Mean" + map.get("mean"));
+        jLMedian.setText("Median" + map.get("median"));
+        jLPercentile.setText("Percentile" + (int) Math.round(map.get("percentile")));
+        jLPixels.setText("Pixels: " + (int) Math.round(map.get("pixels")));
+        jLStdDev.setText("Std dev: " + map.get("std_dev"));
+
+    }
+
+
+
+
+    private Map<String, Double> _calculateStats(Map<String, int[]> ch,String channelName){
+
+
+        Map<String, Double> statsOUT = new HashMap<>();
+
+        BufferedImage buffImg = ImageWindow.toBufferedImage(this.image);
+        int[] pixels = ((DataBufferInt) buffImg.getRaster().getDataBuffer()).getData();
+
+        ArrayList sorted = new ArrayList<Double>();
+
+
+        int sum = 0;
+        int sum_std = 0;
+        double stddev = 0;
+        for ( int i = 0; i < ch.get(channelName).length ;i++ ){
+
+            sum += ch.get(channelName)[i]*i;
+            sum_std += ch.get(channelName)[i];
+            sorted.add(new Double(ch.get(channelName)[i]));
+            //stddev += Math.pow((ch.get("Red")[i] - ((double)sum/(double) ch.get("Red").length) ),2) / ch.get("Red").length;
+
+        }
+
+//        for (int  i = 0; i < pixels.length ;i++ ){
+//
+//            stddev += Math.pow((double) (ch.get("Red")[i] - ((double)sum/(double) pixels.length) ),2);
+//
+//        }
+
+        Collections.sort(sorted);
+
+        statsOUT.put("count",(double)sum);
+        statsOUT.put("pixels",(double)pixels.length);
+        statsOUT.put("percentile",(double)100);
+        statsOUT.put("median", (Double) sorted.get(ch.get(channelName).length/2));
+        statsOUT.put("mean",(double)sum/pixels.length);
+        //statsOUT.put("std_dev",(double)Math.sqrt(stddev/pixels.length));
+
+
+
+        return statsOUT;
+    }
+
+
+
+    private Map<String, int[]> _calculateHist(){
 
         //TYPE_INT_ARGB
 
         //ARGBARGBARGB
 
         BufferedImage buffImg = ImageWindow.toBufferedImage(this.image);
-
-        Map<String, int[]> tmoOUT = new HashMap<>();
 
         int[] dataR = new int[256];
         int[] dataG = new int[256];
@@ -203,8 +316,10 @@ public class Histogram extends JFrame {
 
         int i,notColor=0;
         //Iterate over whole pixel array (each pixel has 4 bytes)
-        for ( i=0; (i+3) < pixels.length ;i+=1 ){
+        for ( i=0; (i+0) < pixels.length ;i+=1 ){
             int r,g,b;
+
+            //A channel can be ommited
             r = ( (pixels[i]) & 0x00ff0000 ) >> 16;
             g = ( (pixels[i]) & 0x0000ff00 ) >> 8;
             b = ( (pixels[i]) & 0x000000ff );
@@ -212,6 +327,7 @@ public class Histogram extends JFrame {
             dataR[ r ]++;
             dataG[ g ]++;
             dataB[ b ]++;
+
             if(((r == g) && (r == b)  )){
                 notColor++;
             }
@@ -221,6 +337,8 @@ public class Histogram extends JFrame {
             this.isColor = false;
         }
 
+
+        this._updateStats(this._calculateStats(tmoOUT,"Red"));
 
 
         return tmoOUT;
@@ -236,7 +354,7 @@ public class Histogram extends JFrame {
         DefaultCategoryDataset datasetB = new DefaultCategoryDataset();
 
         //Set values for given pixel
-        for (int yy=0;yy<255;yy++){
+        for (int yy=0;yy<256;yy++){
             datasetR.setValue(dataRGB.get("Red")[yy],"1",String.valueOf(yy));
             datasetG.setValue(dataRGB.get("Green")[yy],"1",String.valueOf(yy));
             datasetB.setValue(dataRGB.get("Blue")[yy],"1",String.valueOf(yy));
@@ -251,7 +369,7 @@ public class Histogram extends JFrame {
                 datasetR,
 
                 PlotOrientation.VERTICAL, // orientation
-                true,
+                false,
 
                 true,
                 false
@@ -276,7 +394,7 @@ public class Histogram extends JFrame {
                 datasetG,
 
                 PlotOrientation.VERTICAL, // orientation
-                true,
+                false,
 
                 true,
                 false
@@ -297,7 +415,7 @@ public class Histogram extends JFrame {
                 datasetB,
 
                 PlotOrientation.VERTICAL, // orientation
-                true,
+                false,
 
                 true,
                 false
@@ -327,8 +445,8 @@ public class Histogram extends JFrame {
 
          */
 
-        //Add charts to tabs
 
+        //Add charts to tabs
 
         jPTabRed.setLayout(new BorderLayout());
         jPTabGreen.setLayout(new BorderLayout());
@@ -341,7 +459,7 @@ public class Histogram extends JFrame {
         jPTabBlue.setVisible(true);
         this.setMinimumSize((jPTabRed.getComponents()[0].getPreferredSize()));
         this.setMinimumSize(new Dimension(getWidth(),this.getHeight()+100));
-
+        jTabCon.addChangeListener(this);
 
 
     }
@@ -450,4 +568,32 @@ public class Histogram extends JFrame {
 
 
     }
+
+
+
+
+
+
+    /**
+     * Invoked when the target of the listener has changed its state.
+     *
+     * @param e a ChangeEvent object
+     */
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        JTabbedPane tabbedPane = (JTabbedPane) e.getSource();
+        int selectedIndex = tabbedPane.getSelectedIndex();
+        //JOptionPane.showMessageDialog(null, "Selected Index: " + selectedIndex);
+        System.out.println("Selected Index: " + tabbedPane.getTitleAt(tabbedPane.getSelectedIndex()));
+
+        this._updateStats(this._calculateStats(tmoOUT,tabbedPane.getTitleAt(tabbedPane.getSelectedIndex())));
+
+
+
+    }
+
+
+
+
+
 }
